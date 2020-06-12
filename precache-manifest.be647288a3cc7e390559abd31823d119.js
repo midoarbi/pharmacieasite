285a437c4b11f6326c61d5b77f090e695d426c16
midoarbi <elmehdinassiri31@gmail.com>
2020-06-12 18:43:40 +0100

    Updates

---
+++ precache-manifest.be647288a3cc7e390559abd31823d119.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4025fdbd4a9bd4809b21646ef3bb43",
+    "url": "/pharmacieasite/index.html"
+  },
+  {
+    "revision": "b47df696ec521a889f2f",
+    "url": "/pharmacieasite/static/css/main.fe365c14.chunk.css"
+  },
+  {
+    "revision": "b30fd2d0b287b2c87019",
+    "url": "/pharmacieasite/static/js/2.47ead214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pharmacieasite/static/js/2.47ead214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47df696ec521a889f2f",
+    "url": "/pharmacieasite/static/js/main.cfc203be.chunk.js"
+  },
+  {
+    "revision": "59c3cbcc3cdad2ebe84c",
+    "url": "/pharmacieasite/static/js/runtime-main.0adc4159.js"
+  },
+  {
+    "revision": "a17abd0604f2ec6f4bafd91d82ef96de",
+    "url": "/pharmacieasite/static/media/GothamRounded-Bold.a17abd06.otf"
+  },
+  {
+    "revision": "a40ce5cf5abb54c2a01d4dde85de1c5d",
+    "url": "/pharmacieasite/static/media/email.a40ce5cf.png"
+  },
+  {
+    "revision": "1fb657d9cbde8c538c1599a63a011e03",
+    "url": "/pharmacieasite/static/media/inscription_illustration.1fb657d9.png"
+  },
+  {
+    "revision": "f960d720497280564b5ecfe8a18c13e0",
+    "url": "/pharmacieasite/static/media/logo-monpharmacien.f960d720.png"
+  },
+  {
+    "revision": "f960d720497280564b5ecfe8a18c13e0",
+    "url": "/pharmacieasite/static/media/monpharmacie.f960d720.png"
+  },
+  {
+    "revision": "8325191b62681bd467968182d832e9df",
+    "url": "/pharmacieasite/static/media/sprite-site.8325191b.png"
+  },
+  {
+    "revision": "2293d9223cb405109726e72c1e14149f",
+    "url": "/pharmacieasite/static/media/sprite.2293d922.png"
+  }
+]);
