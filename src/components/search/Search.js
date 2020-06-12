5b95d7a1feb196da8bf54f078b608b633722390b
midoarbi <elmehdinassiri31@gmail.com>
2020-06-12 17:53:10 +0100

    Search bar fixed

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -11,27 +11,18 @@ const Search = () => {
               <div className="form_search_controller" >
                <input type="text" id="name" placeholder="Nom de la pharmacie" />
               </div>
+             
               <div className="form_search_controller last" >
-                <select id="searchInput"  name="localisation"   className="form-control input select-city select2-hidden-accessible" tabindex="-1" aria-hidden="true" >
-                    <option>Casablanca</option>
-                </select>
+        
                 <span className="select2 select2-container select2-container--default select2-container--below" dir="ltr" style={{width:'380px'}} >
                   <span className="selection" >
-                      <span className="select2-selection select2-selection--single" 
-                      role="combobox" aria-haspopup="true" aria-expanded="false" 
-                      tabindex="0"
-                      aria-labelledby="select2-searchInput-container"
-                      >
-                          <span className="select2-selection__rendered" id="select2-searchInput-container" >
-                              <span className="select2-selection__placeholder" >Où ? (Ville, quartier...) Selectionner</span>
-                          </span>
+                  <div className="form_search_controller2" >
+               <input type="text" id="name" placeholder="Où ? (Ville, quartier...)" />
+              </div>
                           <span className="select2-selection__arrow" role="presentation">
                              <b role="presentation" ></b>
                           </span>
-                      </span>
                   </span>
-                  <span className="dropdown-wrapper" aria-hidden="true" ></span>
-
                 </span>
               </div>
               <button type="submit" className="submit_search" id="getPharmacies" disabled >Chercher</button>
